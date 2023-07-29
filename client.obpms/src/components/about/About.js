2ee54db8c70a919c5a49e35f6b9eab28a746e331
Mukul Rai <rai.mukul237@gmail.com>
2023-07-29 12:13:12 -0500

    update

--- client.obpms/src/components/about/About.js
+++ client.obpms/src/components/about/About.js
@@ -4,9 +4,9 @@ import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import ButtonBase from "@mui/material/ButtonBase";
-import Divider from '@mui/material/Divider';
-import Chip from '@mui/material/Chip';
-import Avatar from '@mui/material/Avatar';
+import Divider from "@mui/material/Divider";
+import Chip from "@mui/material/Chip";
+import Avatar from "@mui/material/Avatar";
 
 const Img = styled("img")({
   margin: "auto",
@@ -18,55 +18,71 @@ const Img = styled("img")({
 export default function ComplexGrid() {
   return (
     <div>
-       <Divider style={{paddingTop:10,paddingButtom:10 }}>
+      <Divider style={{ paddingTop: 10, paddingButtom: 10 }}>
         <Chip color="warning" avatar={<Avatar>A</Avatar>} label="About Us" />
-      </Divider> 
-    <Paper
-      sx={{
-        p: 2,
-        margin: "auto",
-        maxWidth: 1000,
-        flexGrow: 1,
-        backgroundColor: (theme) =>
-          theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-      }}
-    >
-      <Grid container spacing={2}>
-        <Grid item sx={{
-            margin: 'auto'
-          }}>
-          <ButtonBase sx={{ width: 300, height: 333 }}>
-            <Img alt="complex" src="/about.jpg" />
-          </ButtonBase>
-        </Grid>
-        <Grid item xs={12} sm container>
-          <Grid item container direction="column">
-            <Typography gutterBottom variant="subtitle1" color="text.secondary">
-              The Glitter-Girls. Based in Jaipur, We have been working since
-              2012 & have dolled-up more than 7 brides.
-            </Typography>
-            <Typography variant="subtitle1" gutterBottom color="text.secondary">
-              My goal is to make sure that all my brides have a pleasant
-              experience and a smile on their faces from our very first
-              conversation to the D day where she walks out looking like a
-              million bucks and making heads turn everywhere she goes.
-            </Typography>
-            <Typography variant="subtitle1" gutterBottom color="text.secondary">
-              When my magic brushes and color touch you look at what you would
-              have imagined to look like and even much better.We are highly
-              skilled and dedicated makeup artist with an outstanding record of
-              client satisfaction and customer service.
-            </Typography>
-            <Typography variant="h6" gutterBottom color="text.primary" mt={2}>
-              Srishti
-            </Typography>
-            <Typography variant="p" gutterBottom >
-              <i color="warning">Salon Owner</i>
-            </Typography>
+      </Divider>
+      <Paper
+        sx={{
+          p: 2,
+          margin: "auto",
+          maxWidth: 1000,
+          flexGrow: 1,
+          backgroundColor: (theme) =>
+            theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid
+            item
+            sx={{
+              margin: "auto",
+            }}
+          >
+            <ButtonBase sx={{ width: 300, height: 333 }}>
+              <Img alt="complex" src="/about.jpg" />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item container direction="column">
+              <Typography
+                gutterBottom
+                variant="subtitle1"
+                color="text.secondary"
+              >
+                We believe that beauty is not just skin deep; it is an
+                expression of your inner radiance and confidence. We are
+                passionate about helping you look and feel your best, which is
+                why we have created a haven of beauty and relaxation, where you
+                can indulge in a wide range of services designed to pamper and
+                rejuvenate.
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                gutterBottom
+                color="text.secondary"
+              >
+                Our Story: <br/>Established in 2023, Glitter-Girls has been serving
+                the Overland Park community with exceptional beauty services and
+                treatments. From the beginning, our mission has been to bring
+                out the natural beauty in everyone who walks through our doors.
+                Our team of skilled and experienced beauty professionals are
+                dedicated to delivering personalized services, tailored to meet
+                the unique needs of each individual.
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                gutterBottom
+                color="text.secondary"
+              >
+                When my magic brushes and color touch you look at what you would
+                have imagined to look like and even much better.We are highly
+                skilled and dedicated makeup artist with an outstanding record
+                of client satisfaction and customer service.
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
     </div>
   );
 }
